053b7f3ea933142bd3a60726c893b93e87a78247
Renaudjean <renaud06goubelle@gmail.com>
2022-07-11 17:03:00 +0200

    fixing merge issues

--- app/views/viewCategorie.php
+++ app/views/viewCategorie.php
@@ -4,9 +4,9 @@
         <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">Fantasy</a>
 
         <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+        <?php foreach($CatSeries as $CategoSerie){  ?>
+            <li><a class="dropdown-item" href="<?=$CategoSerie->id?>"><?=$CategoSerie->name?></a></li>
+            <?php } ?>
         </ul>
     </div>
 
