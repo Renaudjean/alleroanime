80483b0b7be40452a9c1f2bf09252b8c54f115f7
Renaudjean <renaud06goubelle@gmail.com>
2022-07-11 09:17:44 +0200

    Moving search to categories to get a list instead of a specific request

--- app/controllers/search.php
+++ app/controllers/search.php
@@ -1,6 +1,6 @@
 <?php 
 class search extends Controller {
-    public function viewInfo(){ 
+    public function viewCategorie(){ 
         $searchMovies = $this->model('searchMovies');
         $this->render('viewCategorie',['seachResult'=>$searchMovies->searchM->results]);
         
