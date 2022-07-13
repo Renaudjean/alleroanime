20b9a9e3a3993d308f3d15cf9aaf389eb1d3369a
Renaudjean <renaud06goubelle@gmail.com>
2022-07-13 15:00:11 +0200

    Categorie laid out

--- app/views/viewCategorie.php
+++ app/views/viewCategorie.php
@@ -15,45 +15,64 @@
 
     <?php 
     //  verify if searchresults carries the data if not skip to the default view of categorie
-     if($searchResults != ''){
+
+    if($searchResults != ''){
      foreach($searchResults as $searchResult){
       // Check which Media we are dealing with for proper parameters use also verify if the genre id has animation 
       if($searchResult->media_type == "tv" && in_array("16", $searchResult->genre_ids)){?>
-        <h6> <?= $searchResult->name; ?> </h6>
-        <p><?= $searchResult->overview;?> </p>
-        <a href="viewInfo.php"><img src="https://image.tmdb.org/t/p/w200/<?=$searchResult->poster_path?>"></a>
-        <br>
+        <div class='counter'>
+        <div class='cat-cards'>
+        <div class='cell2'>
+        <h6 class="title"> <?= $searchResult->name; ?> </h6>
+        <p class="overview"><?= $searchResult->overview;?> </p>
+        </div>
+        <a href="viewInfo.php" class="cover-photo"><img src="https://image.tmdb.org/t/p/w200/<?=$searchResult->poster_path?>"></a>
+        </div>
+        </div>
         <?php }
         
         // Same as first if condition but for movies
         if($searchResult->media_type == "movie" && in_array("16", $searchResult->genre_ids)){?>
-        <h6><?= $searchResult->original_title;?> </h6>
-        <p><?= $searchResult->overview;?> </p>
-        <a href="viewInfo.php"><img src="https://image.tmdb.org/t/p/w200/<?=$searchResult->poster_path?>"></a>
-       <br>
+          <div class='counter'>
+         <div class='cat-cards'>
+          <div class='cell2'>
+          <h6 class="title"><?= $searchResult->original_title;?> </h6>
+          <p class="overview"><?= $searchResult->overview;?> </p>
+        </div>
+        <a href="viewInfo.php" class="cover-photo"><img src="https://image.tmdb.org/t/p/w200/<?=$searchResult->poster_path?>"></a>
+        </div>
+       </div>
        <?php  }
            }
         }
         // verify if SerieGenres and MovieGenres carries the data needed if not ignore to the default view of categorie
         if($SerieGenres != '' && $MovieGenres != ''){
-          
+       
           foreach($SerieGenres as $SerieGenre){?>
-            <h6> <?= $SerieGenre->name; ?> </h6>
-            <p><?= $SerieGenre->overview;?> </p>
-            <a href="viewInfo.php"><img src="https://image.tmdb.org/t/p/w200/<?=$SerieGenre->poster_path?>"></a>
-            <br>
-            
-  <?php $picture =$SerieGenre->poster_path;
-    } 
-    if($picture){
+          <div class='counter'>
+            <div class='cat-cards'> 
+            <div class='cell2'>
+              <h6 class="title"> <?= $SerieGenre->name; ?> </h6>
+              <p class="overview"><?= $SerieGenre->overview;?> </p>
+            </div>
+            <a href="viewInfo.php" class="cover-photo"><img src="https://image.tmdb.org/t/p/w200/<?=$SerieGenre->poster_path?>"></a>
+            </div>
+            </div>
+          <?php $picture =$SerieGenre->poster_path;
+            } 
+            if($picture){
           foreach($MovieGenres as $MovieGenre){?>
-            <h6><?= $MovieGenre->original_title;?> </h6>
-            <p><?= $MovieGenre->overview;?> </p>
-            <a href="viewInfo.php"><img src="https://image.tmdb.org/t/p/w200/<?=$MovieGenre->poster_path?>"></a>
-            <br>
+          <div class='counter'>
+            <div class='cat-cards'>
+              <div class='cell2'>
+                <h6 class="title"><?= $MovieGenre->original_title;?> </h6>
+                <p class="overview"><?= $MovieGenre->overview;?> </p>
+              </div>
+              <a href="viewInfo.php" class="cover-photo"><img src="https://image.tmdb.org/t/p/w200/<?=$MovieGenre->poster_path?>"></a>
+              </div>
+              </div>
             <?php   }
             }
         }?>
 
-    </div>
   </main>
