82bbd12b7d86be8c16d30082ecb115456fe08cc5
Renaudjean <renaud06goubelle@gmail.com>
2022-07-21 09:23:22 +0200

    Fixed info

--- app/controllers/Info.php
+++ app/controllers/Info.php
@@ -1,11 +1,27 @@
 <?php
 
 class Info extends Controller {
-   public function __construct($index = null) {
-    $this->index = $index;
+    
+    public function __construct($index = null) {
+        $this->index = $index;
    }
-
-    public function infoSeries($index){
+    
+    public function infoSeries(){
+        if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on')  {
+            $url = "https://";   
+        }else  {
+            $url = "http://";   
+        // Append the host(domain name, ip) to the URL.   
+        $url.= $_SERVER['HTTP_HOST'];   
+    
+        // Append the requested resource location to the URL   
+        $url.= $_SERVER['REQUEST_URI'];    
+    }
+    $keyserie = "idserie";
+    $keymovie = "idmovie";
+    
+    if( strpos($url, $keyserie) == TRUE ){
+        $index=$_GET['idserie'];
         $ApiInfo = $this->model("ApiInfo");
         $datasSeries = $ApiInfo->getInfoTv($index);
 
@@ -16,14 +32,17 @@ class Info extends Controller {
         $this->render('viewInfo',['infoSeries'=>$datasSeries, 'infoCreditsSeries' => $creditSeries]);
         
     }
-    public function infoMovies($index){
-        $ApiInfoMovies = $this->model("ApiInfoMovies");
-        $datasMovies = $ApiInfoMovies->getInfoM($index);
-        
-        $ApiInfoCreditsMovies = $this->model("ApiInfoMovies");
-        $creditMovies = $ApiInfoCreditsMovies->getCredits($index);
-        // var_dump($datasMovies);
-        // var_dump($creditMovies);
-        $this->render('viewInfo',['infoSeries'=> $datasMovies, 'infoCredits' => $creditMovies]);
-    }
-}  
+
+    if( strpos($url, $keymovie) == TRUE ){
+            $index=$_GET['idmovie'];
+            $ApiInfoMovies = $this->model("ApiInfoMovies");
+            $datasMovies = $ApiInfoMovies->getInfoM($index);
+            
+            $ApiInfoCreditsMovies = $this->model("ApiInfoMovies");
+            $creditMovies = $ApiInfoCreditsMovies->getCredits($index);
+            // var_dump($datasMovies);
+            // var_dump($creditMovies);
+            $this->render('viewInfo',['infoSeries'=> $datasMovies, 'infoCredits' => $creditMovies]);
+         }
+    }  
+}
